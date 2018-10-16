a377655c029dbdbbc38da440c47aad8a9cf091e8
148921 <comtruice@gmail.com>
2018-10-16 19:38:38 +0200

    added bodyParser

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 
 const express = require('express'); // laster inn lib: express
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+
 const app = express(); // gjør express om til en variablel: app
 const port = 3000;
 
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+
 app.get('/', (request, response) => response.send('Hello World')); // get commando for å sjekke "Hello w.."
 
 
